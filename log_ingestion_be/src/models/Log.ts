02cd1c7783318bbf9f5738ca6f696afe65137d3c
Ahmad7420 <ahmadabdulrahman6@gmail.com>
2025-07-05 16:21:38 +0400

    data

--- log_ingestion_be/src/models/Log.ts
+++ log_ingestion_be/src/models/Log.ts
@@ -1,24 +1,3 @@
-import mongoose, { Schema, Document } from 'mongoose';
-import { Log } from '../types/log';
-
-interface ILog extends Log, Document { }
-
-const LogSchema: Schema = new Schema({
-    level: { type: String, required: true },
-    message: { type: String, required: true },
-    resourceId: { type: String, required: true },
-    timestamp: { type: String, default: () => new Date().toISOString() },
-    traceId: { type: String, required: true },
-    spanId: { type: String, required: true },
-    commit: { type: String, required: true },
-    metadata: {
-        parentResourceId: { type: String, required: true },
-    },
-});
-
-export default mongoose.model<ILog>('Log', LogSchema);
-
-
 
 export const sampleLogs = [
     {
